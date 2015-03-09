4f977d19799dc92620c8d5c2ccbbe5b0e0b61893
Paul <paul.fleuryp@gmail.com>
2015-03-09 17:35:37 +0100

    Builds

---
+++ servoPotentio/build-uno/libs/Servo/src/avr/Servo.d
@@ -0,0 +1,15 @@
+build-uno/libs/Servo/src/avr/Servo.o: \
+ /usr/share/arduino/libraries/Servo/src/avr/Servo.cpp \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/USBAPI.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/avr/variants/standard/pins_arduino.h \
+ /usr/share/arduino/libraries/Servo/src/Servo.h \
+ /usr/share/arduino/libraries/Servo/src/avr/ServoTimers.h
---
+++ servoPotentio/build-uno/libs/Servo/src/sam/Servo.d
@@ -0,0 +1,2 @@
+build-uno/libs/Servo/src/sam/Servo.o: \
+ /usr/share/arduino/libraries/Servo/src/sam/Servo.cpp
