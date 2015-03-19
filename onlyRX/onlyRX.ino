56352671a7d86423ec4d6368502b219624e90617
Paul Fleury <paul.fleuryp@gmail.com>
2015-03-19 09:29:20 +0100

    New sketch: only RX

--- onlyRX/onlyRX.ino
+++ onlyRX/onlyRX.ino
@@ -1,14 +1,16 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial serial(0, 0);
+
 void setup() {
-  Serial.begin(9600);
   pinMode(1, OUTPUT);
+  serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(1, HIGH);
-  delay(50);
   digitalWrite(1, LOW);
-  delay(300);
-  if (Serial.available()) {
+  if(serial.available()){
+    serial.read();
     digitalWrite(1, HIGH);
     delay(1000);
   }
