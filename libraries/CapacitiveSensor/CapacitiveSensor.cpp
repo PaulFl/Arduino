5a285c1009c9fe87345983dc330e8f17041d791c
Paul Fleury <paul.fleuryp@gmail.com>
2015-09-21 11:43:40 +0200

    updated libraries

--- libraries/CapacitiveSensor/CapacitiveSensor.cpp
+++ libraries/CapacitiveSensor/CapacitiveSensor.cpp
@@ -1,6 +1,10 @@
 /*
  CapacitiveSense.h v.04 - Capacitive Sensing Library for 'duino / Wiring
+ https://github.com/PaulStoffregen/CapacitiveSensor
+ http://www.pjrc.com/teensy/td_libs_CapacitiveSensor.html
+ http://playground.arduino.cc/Main/CapacitiveSensor
  Copyright (c) 2009 Paul Bagder  All right reserved.
+ Version 05 by Paul Stoffregen - Support non-AVR board: Teensy 3.x, Arduino Due
  Version 04 by Paul Stoffregen - Arduino 1.0 compatibility, issue 146 fix
  vim: set ts=4:
  */
@@ -20,41 +24,35 @@
 
 CapacitiveSensor::CapacitiveSensor(uint8_t sendPin, uint8_t receivePin)
 {
-	uint8_t sPort, rPort;
-
 	// initialize this instance's variables
 	// Serial.begin(9600);		// for debugging
 	error = 1;
 	loopTimingFactor = 310;		// determined empirically -  a hack
-	
+
 	CS_Timeout_Millis = (2000 * (float)loopTimingFactor * (float)F_CPU) / 16000000;
 	CS_AutocaL_Millis = 20000;
-    
+
 	// Serial.print("timwOut =  ");
 	// Serial.println(CS_Timeout_Millis);
-	
+
 	// get pin mapping and port for send Pin - from PinMode function in core
 
 #ifdef NUM_DIGITAL_PINS
 	if (sendPin >= NUM_DIGITAL_PINS) error = -1;
 	if (receivePin >= NUM_DIGITAL_PINS) error = -1;
 #endif
-	
-	sBit =  digitalPinToBitMask(sendPin);			// get send pin's ports and bitmask
-	sPort = digitalPinToPort(sendPin);
-	sReg = portModeRegister(sPort);
-	sOut = portOutputRegister(sPort);				// get pointer to output register   
 
-	rBit = digitalPinToBitMask(receivePin);			// get receive pin's ports and bitmask 
-	rPort = digitalPinToPort(receivePin);
-	rReg = portModeRegister(rPort);
-	rIn  = portInputRegister(rPort);
-   	rOut = portOutputRegister(rPort);
-	
+	pinMode(sendPin, OUTPUT);						// sendpin to OUTPUT
+	pinMode(receivePin, INPUT);						// receivePin to INPUT
+	digitalWrite(sendPin, LOW);
+
+	sBit =  digitalPinToBitMask(sendPin);			// get send pin's ports and bitmask
+	sReg = PIN_TO_BASEREG(sendPin);					// get pointer to output register
+
+	rBit = digitalPinToBitMask(receivePin);			// get receive pin's ports and bitmask
+	rReg = PIN_TO_BASEREG(receivePin);
+
 	// get pin mapping and port for receive Pin - from digital pin functions in Wiring.c
-    noInterrupts();
-	*sReg |= sBit;              // set sendpin to OUTPUT 
-    interrupts();
 	leastTotal = 0x0FFFFFFFL;   // input large value for autocalibrate begin
 	lastCal = millis();         // set millis for start
 }
@@ -86,7 +84,7 @@ long CapacitiveSensor::capacitiveSensor(uint8_t samples)
 			leastTotal = 0x0FFFFFFFL;          // reset for "autocalibrate"
 			lastCal = millis();
 		}
-		/*else{                                // debugging 
+		/*else{                                // debugging
 			Serial.print("  total =  ");
 			Serial.print(total);
 
@@ -98,11 +96,11 @@ long CapacitiveSensor::capacitiveSensor(uint8_t samples)
 			Serial.print(x);
 			Serial.print("     .1 * leastTotal = ");
 			x = (int)(.1 * (float)leastTotal);
-			Serial.println(x);   
+			Serial.println(x);
 		} */
 
 	// routine to subtract baseline (non-sensed capacitance) from sensor return
-	if (total < leastTotal) leastTotal = total;                 // set floor value to subtract from sensed value         
+	if (total < leastTotal) leastTotal = total;                 // set floor value to subtract from sensed value
 	return(total - leastTotal);
 
 }
@@ -139,39 +137,46 @@ void CapacitiveSensor::set_CS_Timeout_Millis(unsigned long timeout_millis){
 int CapacitiveSensor::SenseOneCycle(void)
 {
     noInterrupts();
-	*sOut &= ~sBit;        // set Send Pin Register low
-	
-	*rReg &= ~rBit;        // set receivePin to input
-	*rOut &= ~rBit;        // set receivePin Register low to make sure pullups are off
-	
-	*rReg |= rBit;         // set pin to OUTPUT - pin is now LOW AND OUTPUT
-	*rReg &= ~rBit;        // set pin to INPUT 
-
-	*sOut |= sBit;         // set send Pin High
+	DIRECT_WRITE_LOW(sReg, sBit);	// sendPin Register low
+	DIRECT_MODE_INPUT(rReg, rBit);	// receivePin to input (pullups are off)
+	DIRECT_MODE_OUTPUT(rReg, rBit); // receivePin to OUTPUT
+	DIRECT_WRITE_LOW(rReg, rBit);	// pin is now LOW AND OUTPUT
+	delayMicroseconds(10);
+	DIRECT_MODE_INPUT(rReg, rBit);	// receivePin to input (pullups are off)
+	DIRECT_WRITE_HIGH(sReg, sBit);	// sendPin High
     interrupts();
 
-	while ( !(*rIn & rBit)  && (total < CS_Timeout_Millis) ) {  // while receive pin is LOW AND total is positive value
+	while ( !DIRECT_READ(rReg, rBit) && (total < CS_Timeout_Millis) ) {  // while receive pin is LOW AND total is positive value
 		total++;
 	}
-    
+	//Serial.print("SenseOneCycle(1): ");
+	//Serial.println(total);
+
 	if (total > CS_Timeout_Millis) {
 		return -2;         //  total variable over timeout
 	}
-   
-	// set receive pin HIGH briefly to charge up fully - because the while loop above will exit when pin is ~ 2.5V 
-    noInterrupts();
-	*rOut  |= rBit;        // set receive pin HIGH - turns on pullup 
-	*rReg |= rBit;         // set pin to OUTPUT - pin is now HIGH AND OUTPUT
-	*rReg &= ~rBit;        // set pin to INPUT 
-	*rOut  &= ~rBit;       // turn off pullup
 
-	*sOut &= ~sBit;        // set send Pin LOW
+	// set receive pin HIGH briefly to charge up fully - because the while loop above will exit when pin is ~ 2.5V
+    noInterrupts();
+	DIRECT_WRITE_HIGH(rReg, rBit);
+	DIRECT_MODE_OUTPUT(rReg, rBit);  // receivePin to OUTPUT - pin is now HIGH AND OUTPUT
+	DIRECT_WRITE_HIGH(rReg, rBit);
+	DIRECT_MODE_INPUT(rReg, rBit);	// receivePin to INPUT (pullup is off)
+	DIRECT_WRITE_LOW(sReg, sBit);	// sendPin LOW
     interrupts();
 
-	while ( (*rIn & rBit) && (total < CS_Timeout_Millis) ) {  // while receive pin is HIGH  AND total is less than timeout
+#ifdef FIVE_VOLT_TOLERANCE_WORKAROUND
+	DIRECT_MODE_OUTPUT(rReg, rBit);
+	DIRECT_WRITE_LOW(rReg, rBit);
+	delayMicroseconds(10);
+	DIRECT_MODE_INPUT(rReg, rBit);	// receivePin to INPUT (pullup is off)
+#else
+	while ( DIRECT_READ(rReg, rBit) && (total < CS_Timeout_Millis) ) {  // while receive pin is HIGH  AND total is less than timeout
 		total++;
 	}
-	// Serial.println(total);
+#endif
+	//Serial.print("SenseOneCycle(2): ");
+	//Serial.println(total);
 
 	if (total >= CS_Timeout_Millis) {
 		return -2;     // total variable over timeout
--- libraries/CapacitiveSensor/examples/CapacitiveSensorSketch/CapacitiveSensorSketch.pde
+++ libraries/CapacitiveSensor/examples/CapacitiveSensorSketch/CapacitiveSensorSketch.pde
@@ -10,18 +10,30 @@
 
 
 CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+CapacitiveSensor   cs_4_6 = CapacitiveSensor(4,6);        // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil
+CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
 
 void setup()                    
 {
+   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
    Serial.begin(9600);
 }
 
 void loop()                    
 {
-    long total1 =  cs_4_2.capacitiveSensor();
+    long start = millis();
+    long total1 =  cs_4_2.capacitiveSensor(30);
+    long total2 =  cs_4_6.capacitiveSensor(30);
+    long total3 =  cs_4_8.capacitiveSensor(30);
 
+    Serial.print(millis() - start);        // check on performance in milliseconds
+    Serial.print("\t");                    // tab character for debug windown spacing
 
-    Serial.println(total1);                  // print sensor output 1
+    Serial.print(total1);                  // print sensor output 1
+    Serial.print("\t");
+    Serial.print(total2);                  // print sensor output 2
+    Serial.print("\t");
+    Serial.println(total3);                // print sensor output 3
 
     delay(10);                             // arbitrary delay to limit data to serial port 
 }
