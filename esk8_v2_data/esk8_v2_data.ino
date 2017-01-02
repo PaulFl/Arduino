4e8d52ddb47cadc4e948007aaddd3c0ed6cb73e2
Paul Fleury <paul.fleuryp@gmail.com>
2017-01-02 11:19:55 +0100

    update

--- esk8_v2_data/esk8_v2_data.ino
+++ esk8_v2_data/esk8_v2_data.ino
@@ -28,17 +28,20 @@ struct bldcMeasure measuredValues;
 void setup() {
   SERIALIO.begin(9600); //Vesc serial (Serial)
   lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Voltage: ");
+  lcd.setCursor(0, 1);
+  lcd.print("Ah drawn: ");
 }
 
 void loop() {
   delay(25);
   if (VescUartGetValue(measuredValues)) {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Voltage: ");
+    lcd.setCursor(9,0);
     lcd.print(measuredValues.inpVoltage);
-    lcd.setCursor(0, 1);
-    lcd.print("Ah drawn: ");
+    lcd.print("      ");
+    lcd.setCursor(10, 1);
     lcd.print(measuredValues.ampHours);
+    lcd.print("      ");
   }
 }
