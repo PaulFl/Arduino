1b109068b75acf4515f90e475b58e60d5af76dbb
Paul Fleury <paul.fleuryp@gmail.com>
2017-01-15 18:46:11 +0100

    update

--- esk8_v2_data/esk8_v2_data.ino
+++ esk8_v2_data/esk8_v2_data.ino
@@ -15,6 +15,11 @@ int page = 0;
 int input;
 int maxSpeed = 0;
 
+long stoppedTime = 0;
+long rollingTime = 0;
+long lastStop;
+long startTime;
+
 struct bldcMeasure measuredValues;
 SoftwareSerial sensitiveLink(A1, A2);
 /* Values:
@@ -29,6 +34,7 @@ SoftwareSerial sensitiveLink(A1, A2);
     tachometerAbs
 */
 
+
 void setup() {
   sensitiveLink.begin(9600);
   SERIALIO.begin(9600); //Vesc serial (Serial)
@@ -37,14 +43,22 @@ void setup() {
   lcd.print("Voltage: ");
   lcd.setCursor(0, 1);
   lcd.print("Ah drawn: ");
+  lastStop = millis();
+  startTime = millis();
 }
 
 void loop() {
   delay(5);
   if (VescUartGetValue(measuredValues)) {
+
     if (measuredValues.rpm * 60 * 15 * 3.1415926536 * 0.000083 / 36 / 7 > maxSpeed) {
       maxSpeed = measuredValues.rpm * 60 * 15 * 3.1415926536 * 0.000083 / 36 / 7;
     }
+    if (measuredValues.rpm * 60 * 15 * 3.1415926536 * 0.000083 / 36 / 7 <= 2) {
+      stoppedTime += 5;
+    }
+    rollingTime = millis() - startTime - stoppedTime;
+
     switch (page) {
       case 0:
         lcd.setCursor(0, 0);
@@ -73,7 +87,7 @@ void loop() {
         lcd.print("kph        ");
         lcd.setCursor(0, 1);
         lcd.print("Dist: ");
-        lcd.print((measuredValues.tachometer / 14) * 15 / 36 * 3.1415926536 * 0.000083);
+        lcd.print((measuredValues.tachometer / 44) * 15 / 36 * 3.1415926536 * 0.000083);
         lcd.print("km        ");
         break;
       case 3:
@@ -86,6 +100,16 @@ void loop() {
         lcd.print(measuredValues.ampHoursCharged);
         lcd.print("Ah        ");
         break;
+      case 4:
+        lcd.setCursor(0, 0);
+        lcd.print("Time: ");
+        lcd.print((float)rollingTime / 1000.0 / 60.0);
+        lcd.print("min        ");
+        lcd.setCursor(0, 1);
+        lcd.print("Avg Speed: ");
+        lcd.print((measuredValues.tachometer / 44) * 15 / 36 * 3.1415926536 * 0.000083 / ((float)rollingTime / 1000.0 / 60.0 / 60.0));
+        lcd.print("        ");
+        break;
 
     }
   }
@@ -96,14 +120,14 @@ void loop() {
     }
     if (input == 2) {
       page++;
-      if (page == 4) {
+      if (page == 5) {
         page = 0;
       }
     }
     if (input == 1) {
       page --;
       if (page == -1) {
-        page = 3;
+        page = 4;
       }
     }
   }
