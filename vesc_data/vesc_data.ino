0d674bc1a594a7c55407b320322cf21c3ce52893
Paul Fleury <paul.fleuryp@gmail.com>
2017-10-21 12:46:30 +0200

    up

--- vesc_data/vesc_data.ino
+++ vesc_data/vesc_data.ino
@@ -4,6 +4,9 @@
 #include <local_datatypes.h>
 #include <printf.h>
 #include <VescUart.h>
+
+#include <SoftwareSerial.h>
+
  /* Values:
   *  avgMotorCurrent
   *  avgInputCurrent
@@ -15,15 +18,19 @@
   *  tachometer
   *  tachometerAbs  
   */
-
+#define SERIALIO vescSerial
 struct bldcMeasure measuredValues;
 
+SoftwareSerial vescSerial(7,8); //rx, tx
+
 void setup() {
-  Serial.begin(9600); //Vesc serial
+  Serial.begin(9600); 
+  vescSerial.begin(9600);
+  Serial.println("initialisation");
 }
 
 void loop() {
   if (VescUartGetValue(measuredValues)){
-    
+    //Serial.println(measuredValues.inpVoltage);
   }
 }
