d6074f40c78d7c0d5fb7a6d71638f6459559e5c3
Paul Fleury <paul.fleuryp@gmail.com>
2018-09-15 19:21:52 +0200

    up

--- flaperons/flaperons.ino
+++ flaperons/flaperons.ino
@@ -32,36 +32,36 @@ int leftAileron;
 int rightAileron;
 
 void setup() {
-  Serial.begin(9600);
-  left.attach(leftPin);
-  right.attach(rightPin);
-
-  pinMode(flapsPin, INPUT);
-  pinMode(aileronsPin, INPUT);
+    Serial.begin(9600);
+    left.attach(leftPin);
+    right.attach(rightPin);
+    
+    pinMode(flapsPin, INPUT);
+    pinMode(aileronsPin, INPUT);
 }
 
 void loop() {
-  ailerons = pulseIn(aileronsPin, HIGH);
-  lastValues[lissageArrayIndex][0] = ailerons;
-  flaps = pulseIn(flapsPin, HIGH);
-  lastValues[lissageArrayIndex][1] = flaps;
-  lissageArrayIndex ++;
-  if (lissageArrayIndex == LISSAGE){
-    lissageArrayIndex = 0;
-  }
-  ailerons = 0;
-  flaps = 0;
-  for (int i = 0; i<LISSAGE; i++){
-    ailerons += lastValues[i][0];
-    flaps += lastValues[i][1];
-   }
-   ailerons = ailerons / LISSAGE;
-   flaps = flaps / LISSAGE;
-  ailerons = map(ailerons, aileronsPulseMin, aileronsPulseMax, -90, 90);
-  flaps = map(flaps, flapsPulseMin, flapsPulseMax, flapsMin, flapsMax);
-  leftAileron = flaps + ailerons;
-  rightAileron = flaps - ailerons;
-  Serial.println(leftAileron);
-  left.write(leftAileron);
-  right.write(rightAileron);
+    ailerons = pulseIn(aileronsPin, HIGH);
+    lastValues[lissageArrayIndex][0] = ailerons;
+    flaps = pulseIn(flapsPin, HIGH);
+    lastValues[lissageArrayIndex][1] = flaps;
+    lissageArrayIndex ++;
+    if (lissageArrayIndex == LISSAGE){
+        lissageArrayIndex = 0;
+    }
+    ailerons = 0;
+    flaps = 0;
+    for (int i = 0; i<LISSAGE; i++){
+        ailerons += lastValues[i][0];
+        flaps += lastValues[i][1];
+    }
+    ailerons = ailerons / LISSAGE;
+    flaps = flaps / LISSAGE;
+    ailerons = map(ailerons, aileronsPulseMin, aileronsPulseMax, -90, 90);
+    flaps = map(flaps, flapsPulseMin, flapsPulseMax, flapsMin, flapsMax);
+    leftAileron = flaps + ailerons;
+    rightAileron = flaps - ailerons;
+    Serial.println(leftAileron);
+    left.write(leftAileron);
+    right.write(rightAileron);
 }
