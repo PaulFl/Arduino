89f6e8360383e93330cf25ac21abcab9b40c2651
Paul Fleury <paul.fleuryp@gmail.com>
2018-09-15 19:21:03 +0200

    lib update

---
+++ neo_6m_gps/neo_6m_gps.ino
@@ -0,0 +1,16 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial gpsReceiver(3,4);
+
+void setup() {
+  Serial.begin(9600);
+  gpsReceiver.begin(9600);
+}
+
+void loop() {
+  while(gpsReceiver.available() > 0){
+    byte gpsData = gpsReceiver.read();
+    Serial.write(gpsData);
+  }
+}
+
