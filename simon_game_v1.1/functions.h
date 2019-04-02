2e186dcb1ff78174474571365cc22e74d7155558
Paul Fleury <paul.fleuryp@gmail.com>
2019-04-02 14:22:37 +0200

    simon new version with best score reset and name
    tested a bit, seems fine

---
+++ simon_game_v1.1/functions.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONS
+#define FUNCTIONS
+
+boolean ajouterEtape();
+void playSequence(boolean finished);
+int checkEtape(char etape);
+void lectureBoutons();
+void animationReponseFausse(int lastPressed);
+void ecranFin(boolean perdu);
+void flip();
+void delayFlip(unsigned int delay);
+void display(byte text[2]);
+void resetScore();
+
+#endif
