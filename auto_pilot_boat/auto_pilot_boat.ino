1e32c5bae0d43400047a0c835893ae942a4ad1f5
Paul Fleury <paul.fleuryp@gmail.com>
2019-04-04 10:14:29 +0200

    pid fix + re indent

--- auto_pilot_boat/auto_pilot_boat.ino
+++ auto_pilot_boat/auto_pilot_boat.ino
@@ -49,86 +49,90 @@ int telecommande_cap;
 
 void setup()
 {
-  Serial.begin(9600);  // Start serial port
-  Wire.begin();
-
-  pinMode(pin_in1, OUTPUT);
-  pinMode(pin_in2, OUTPUT);
-  pinMode(pin_in3, OUTPUT);
-  pinMode(pin_in4, OUTPUT);
-  pinMode(pin_fna, OUTPUT);
-  pinMode(pin_fnb, OUTPUT);
+    Serial.begin(9600);  // Start serial port
+    Wire.begin();
+    
+    pinMode(pin_in1, OUTPUT);
+    pinMode(pin_in2, OUTPUT);
+    pinMode(pin_in3, OUTPUT);
+    pinMode(pin_in4, OUTPUT);
+    pinMode(pin_fna, OUTPUT);
+    pinMode(pin_fnb, OUTPUT);
 }
 
 void loop()
 {
-  lire_telecommande();
-
-  if (telecommande_switch > 1500) {
-    asservir = true;
-  }
-
-  Wire.beginTransmission(CMPS12_ADDRESS);
-  Wire.write(1); //Skip version
-  Wire.endTransmission();
-
-  Wire.requestFrom(CMPS12_ADDRESS, 30);
-  Wire.read(); //Skip 8bit heading
-
-  int heading = Wire.read();
-  heading <<= 8;
-  heading += Wire.read();
-  cap = heading / 10;
-
-  e_k = cap - CONSIGNE_CAP; //il faudra réflechir à comment gerer le passage de 359,9 à 0 degrés, dans le cas d'une navigation vers le nord
-
-  u_k = u_k_1 + Kp * (e_k * (1 + Ki * dt + Kd / dt) + e_k_1 * (-1 - 2 * Kd / dt) + e_k_2 * (Kd / dt)); //PID
-
-  delay(dt);
-  //SI u_k positif -> vers la droite, si u_k négatif -> vers la gauche
-
-  if (asservir) {
-    vitesse_moteur_1 = CONSIGNE_VITESSE + u_k;
-    vitesse_moteur_2 = CONSIGNE_VITESSE - u_k;
-  } else {
-    int vitesse = (telecommande_vitesse - 1500) / 5;
-    int difference_vitesse = (telecommande_cap - 1500) / 5;
-
-    vitesse_moteur_1 = vitesse + difference_vitesse;
-    vitesse_moteur_2 = vitesse - difference_vitesse;
-  }
-  activer_moteurs(vitesse_moteur_1, vitesse_moteur_2);
-
-
+    lire_telecommande();
+    
+    if (telecommande_switch > 1500) {
+        asservir = true;
+    }
+    
+    Wire.beginTransmission(CMPS12_ADDRESS);
+    Wire.write(1); //Skip version
+    Wire.endTransmission();
+    
+    Wire.requestFrom(CMPS12_ADDRESS, 30);
+    Wire.read(); //Skip 8bit heading
+    
+    int heading = Wire.read();
+    heading <<= 8;
+    heading += Wire.read();
+    cap = heading / 10;
+    
+    e_k = cap - CONSIGNE_CAP; //il faudra réflechir à comment gerer le passage de 359,9 à 0 degrés, dans le cas d'une navigation vers le nord
+    
+    u_k = u_k_1 + Kp * (e_k * (1 + Ki * dt + Kd / dt) + e_k_1 * (-1 - 2 * Kd / dt) + e_k_2 * (Kd / dt)); //PID
+    
+    u_k_1 = u_k;
+    e_k_2 = e_k_1;
+    e_k_1 = e_k;
+    
+    delay(dt);
+    //SI u_k positif -> vers la droite, si u_k négatif -> vers la gauche
+    
+    if (asservir) {
+        vitesse_moteur_1 = CONSIGNE_VITESSE + u_k;
+        vitesse_moteur_2 = CONSIGNE_VITESSE - u_k;
+    } else {
+        int vitesse = (telecommande_vitesse - 1500) / 5;
+        int difference_vitesse = (telecommande_cap - 1500) / 5;
+        
+        vitesse_moteur_1 = vitesse + difference_vitesse;
+        vitesse_moteur_2 = vitesse - difference_vitesse;
+    }
+    activer_moteurs(vitesse_moteur_1, vitesse_moteur_2);
+    
+    
 }
 
 void lire_telecommande() {
-  telecommande_vitesse = ppm.rawChannelValue(1);
-  telecommande_cap = ppm.rawChannelValue(4);
-  telecommande_switch = ppm.rawChannelValue(5);
+    telecommande_vitesse = ppm.rawChannelValue(1);
+    telecommande_cap = ppm.rawChannelValue(4);
+    telecommande_switch = ppm.rawChannelValue(5);
 }
 
 
 
 void activer_moteurs(int vitesse_m1, int vitesse_m2) {
-  if (vitesse_moteur_1 > 0) {
-    digitalWrite(pin_in1, HIGH);
-    digitalWrite(pin_in2, LOW);
-  } else {
-    digitalWrite(pin_in1, LOW);
-    digitalWrite(pin_in2, HIGH);
-  }
-  if (vitesse_moteur_2 > 0) {
-    digitalWrite(pin_in3, HIGH);
-    digitalWrite(pin_in4, LOW);
-  } else {
-    digitalWrite(pin_in3, LOW);
-    digitalWrite(pin_in4, HIGH);
-  }
-
-  int vitesse_m1_abs = map(abs(vitesse_moteur_1), 0, 100, 0, 255 );
-  int vitesse_m2_abs = map(abs(vitesse_moteur_2), 0, 100, 0, 255 );
-
-  analogWrite(pin_fna, vitesse_m1_abs);
-  analogWrite(pin_fnb, vitesse_m2_abs);
+    if (vitesse_moteur_1 > 0) {
+        digitalWrite(pin_in1, HIGH);
+        digitalWrite(pin_in2, LOW);
+    } else {
+        digitalWrite(pin_in1, LOW);
+        digitalWrite(pin_in2, HIGH);
+    }
+    if (vitesse_moteur_2 > 0) {
+        digitalWrite(pin_in3, HIGH);
+        digitalWrite(pin_in4, LOW);
+    } else {
+        digitalWrite(pin_in3, LOW);
+        digitalWrite(pin_in4, HIGH);
+    }
+    
+    int vitesse_m1_abs = map(abs(vitesse_moteur_1), 0, 100, 0, 255 );
+    int vitesse_m2_abs = map(abs(vitesse_moteur_2), 0, 100, 0, 255 );
+    
+    analogWrite(pin_fna, vitesse_m1_abs);
+    analogWrite(pin_fnb, vitesse_m2_abs);
 }
