4e5f6de71f090f662694adfa821540beb64e744e
Paul Fleury <paul.fleuryp@gmail.com>
2019-04-12 23:07:56 +0200

    avrdude upload

--- simon_game/Software/burn_soft.sh
+++ simon_game/Software/burn_soft.sh
@@ -1,1 +1,1 @@
-avrdude -v -V -patmega328p -carduino -P/dev/cu.usbmodem142301 -b19200 -Uflash:w:Google\ Drive/Projects/Simon/Software/simon_game_v1.1.ino.hex:i
+sudo avrdude -p m328p -c linuxspi -v -P /dev/spidev0.0 -U flash:w:simon_game_2.5.hex
