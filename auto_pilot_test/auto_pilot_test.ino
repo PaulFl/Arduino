74e652e2718e8c90db1917469c65f15ef7440112
Paul Fleury <paul.fleuryp@gmail.com>
2019-05-19 09:44:59 +0200

    new sketch

---
+++ auto_pilot_test/auto_pilot_test.ino
@@ -0,0 +1,32 @@
+#include <Servo.h>
+
+short servo_1_select = A0;
+short servo_2_select = A1;
+
+short servo_1_ap = 5;
+short servo_2_ap = 9;
+
+Servo servo_1;
+Servo servo_2;
+
+void setup() {
+	pinMode(servo_1_select, OUTPUT);
+	pinMode(servo_2_select, OUTPUT);
+
+	digitalWrite(servo_1_select, HIGH);
+	digitalWrite(servo_2_select, HIGH);
+
+	servo_1.attach(servo_1_ap);
+	servo_2.attach(servo_2_ap);
+
+}
+
+void loop() {
+	servo_1.write(40);
+	servo_2.write(40);
+	delay(2000);
+	servo_1.write(120);
+	servo_1.write(120);
+	delay(2000);
+
+}
