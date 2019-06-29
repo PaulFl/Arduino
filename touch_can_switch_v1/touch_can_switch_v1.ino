bd689171d2fa88cc0d41529233d2f85cb37bb924
Paul Fleury <paul.fleuryp@gmail.com>
2019-06-29 23:08:25 +0200

    new sketch

---
+++ touch_can_switch_v1/touch_can_switch_v1.ino
@@ -0,0 +1,24 @@
+#include <CapacitiveSensor.h>
+
+int sensor_send_pin = 2;
+int sensor_rcv_pin = 3;
+
+int leds_pin[] = {9,10,11};
+int status_led_pin = 13;
+int pot_pin = A0;
+
+
+void setup() {
+
+}
+
+void loop() {
+}
+
+void cap_sensor() {
+
+}
+
+void set_threshold() {
+
+}
